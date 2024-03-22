0b15c5a75afe4b8004212d00740d2bde66de9cca
HuijeWoo <152831616+HuijeWoo@users.noreply.github.com>
2024-03-22 09:29:37 +0900

    Ah
    innner ... sg ... egress... 0.0.0.0/0 왜 안 넣었어...

--- load_balancer.tf
+++ load_balancer.tf
@@ -18,7 +18,10 @@ resource "aws_lb" "inner_lb" {
   load_balancer_type = "application"
   security_groups    = [aws_security_group.inner_lb_sg.id]
   subnets            = aws_subnet.web_private[*].id
-
+  ##  
+  ip_address_type = "ipv4"
+  
+  ##
   depends_on = [
     aws_security_group.inner_lb_sg,
     aws_lb_target_group.inner_lb_tg,
--- security_group_2.tf
+++ security_group_2.tf
@@ -41,6 +41,7 @@ resource "aws_security_group" "inner_lb_sg" {
     from_port = 0
     to_port   = 0
     protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
